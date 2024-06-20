fbd5080b50a193acb8f4dc6a20eced145c5982f9
Daffeney <145998665+Daffeney89@users.noreply.github.com>
2024-06-21 00:47:31 +0100

    Create GlueJobSetup.java
    aws glue

---
+++ src/Glue/GlueJobSetup.java
@@ -0,0 +1,8 @@
+package com.example.glue;
+
+public class GlueJobSetup {
+    public void setupGlueJob() {
+        System.out.println("Configuring AWS Glue ETL jobs...");
+        // Logic to set up and configure Glue ETL jobs for data transformation
+    }
+}
