b84357f1b2b14a34929894ad14a223a169ebf905
Daffeney <145998665+Daffeney89@users.noreply.github.com>
2024-06-21 00:48:25 +0100

    Create PostgresSetup.java
    RDS setup

---
+++ src/RDS/PostgresSetup.java
@@ -0,0 +1,8 @@
+package com.example.rds;
+
+public class PostgresSetup {
+    public void setupPostgresDB() {
+        System.out.println("Setting up PostgreSQL on Amazon RDS...");
+        // Logic to configure and deploy PostgreSQL instances on RDS
+    }
+}
